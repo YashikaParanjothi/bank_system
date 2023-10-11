bd0b13fcfb150eb0c40a339e50395b7a59213884
Yashika P <66018962+YashikaParanjothi@users.noreply.github.com>
2023-10-12 00:28:52 +0530

    Update Node_1.cpp

--- DSAproject/Node_1.cpp
+++ DSAproject/Node_1.cpp
@@ -1,15 +1,9 @@
-
 # include "Node_1.h"
 
-Node_1::Node_1()
+Node_1::Node_1() : next(nullptr), accountNumber(0), password(0)
 {
-	next = nullptr;
-	accountNumber = 0;
-	password = 0;
 }
-Node_1::Node_1(int a, int p)
+
+Node_1::Node_1(int a, int p) : next(nullptr), accountNumber(a), password(p)
 {
-	next = nullptr;
-	accountNumber = a;
-	password = p;
 }
