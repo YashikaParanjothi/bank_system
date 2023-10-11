2fe2ede262fd8897d98b4149b4f30302c1e8bc28
Yashika P <66018962+YashikaParanjothi@users.noreply.github.com>
2023-10-12 00:47:02 +0530

    Update BST_Node.cpp

--- DSAproject/BST_Node.cpp
+++ DSAproject/BST_Node.cpp
@@ -1,12 +1,8 @@
 # include "BST_Node.h"
 
-BST_Node::BST_Node() : name(""), adress("")
-{
-}
+BST_Node::BST_Node() = default;
 
 BST_Node::BST_Node(string name, string adress, int accountno, int password, int balance)
     : name(name), adress(adress), account_number(accountno), password(password), balance(balance)
 {
 }
-BST_Node::BST_Node() = default;
-BST_Node::BST_Node(const BST_Node&) = default;
