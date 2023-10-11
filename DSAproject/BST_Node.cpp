0ef1747a0e80c0021d73872ac76e100b6a8ed4b3
Yashika P <66018962+YashikaParanjothi@users.noreply.github.com>
2023-10-12 00:44:10 +0530

    Update BST_Node.cpp

--- DSAproject/BST_Node.cpp
+++ DSAproject/BST_Node.cpp
@@ -1,10 +1,18 @@
 # include "BST_Node.h"
 
-BST_Node::BST_Node() : name(""), adress("")
+BST_Node::BST_Node() : name(""), adress(""), balance(0)
 {
+    // Default implementation for the default constructor
 }
 
 BST_Node::BST_Node(string name, string adress, int accountno, int password, int balance)
-    :  name(name), adress(adress), account_number(accountno), password(password), balance(balance)
+    : name(name), adress(adress), account_number(accountno), password(password), balance(balance)
 {
+    // Default implementation for the parameterized constructor
 }
+
+// Use "=default" for the default constructor
+BST_Node::BST_Node() = default;
+
+// Use "=default" for the copy constructor
+BST_Node::BST_Node(const BST_Node&) = default;
